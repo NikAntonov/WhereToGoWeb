4688402b98085fe8993fb88a985d4a24fdf30c5f
CronSystems <cronhelp@gmail.com>
2017-01-29 11:18:27 +0200

    Redesign + Admin

--- js/parallax.js
+++ js/parallax.js
@@ -10,6 +10,17 @@ function parallaxmain(){
     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
 }
 
+function parallaxsoon(){
+    var jumboHeight = $('#jumbotron-soon').outerHeight();
+    //noinspection JSValidateTypes,JSUnresolvedFunction
+    var scrolled = $(window).scrollTop();
+    //noinspection JSUnresolvedFunction
+    $('.bgsoon').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+
+
 $(window).scroll(function(e){
     parallaxmain();
+    parallaxsoon();
 });
